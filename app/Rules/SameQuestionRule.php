f753d9def020e22c5143e1d8466cb4ab695eb62c
gabrielfpereira <gabrielf.pereira@outlook.com>
2023-08-01 21:09:47 -0300

    validação customizada para saber se pergunta já existe

---
+++ app/Rules/SameQuestionRule.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Rules;
+
+use App\Models\Question;
+use Closure;
+use Illuminate\Contracts\Validation\ValidationRule;
+
+class SameQuestionRule implements ValidationRule
+{
+    /**
+     * Run the validation rule.
+     *
+     * @param  \Closure(string): \Illuminate\Translation\PotentiallyTranslatedString  $fail
+     */
+    public function validate(string $attribute, mixed $value, Closure $fail): void
+    {
+        if($this->validationRule($value)) {
+            $fail('this questions already exists');
+        }
+    }
+
+    private function validationRule(string $value): bool
+    {
+        return Question::whereQuestion($value)->exists();
+
+    }
+}
